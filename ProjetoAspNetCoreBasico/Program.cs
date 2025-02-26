a91b770d512acf3c363f2025814355a15ba11ad5
wilson <wilson.lima.silva@hotmail.com>
2025-02-25 23:29:22 -0300

    Adicionado configurações na classe Program.cs para configurar a inicialização da pagina html para reenderiza-la.

--- ProjetoAspNetCoreBasico/Program.cs
+++ ProjetoAspNetCoreBasico/Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+// Servir arquivos estáticos (HTML, CSS, JS)
+app.UseStaticFiles();
+
+// Definir a página inicial como index.html
+app.MapGet("/", async (HttpContext context) =>
+{
+    context.Response.ContentType = "text/html"; // Definir o tipo de conteúdo como HTML
+    await context.Response.SendFileAsync("wwwroot/index.html");
+});
 
 app.Run();
